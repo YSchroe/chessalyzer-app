61cf946b93b95fec438df2b38aa7fa40a8393a08
PeterPain <5794325+YSchroe@users.noreply.github.com>
2023-06-24 12:13:57 +0200

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,7 @@
-<script setup lang="ts">
-    import { ref } from 'vue'
-
-    defineProps<{ msg: string }>()
-
-    const count = ref(0)
-</script>
-
 <template>
     <h1>{{ msg }}</h1>
 
-    <div class="card">
+    <div>
         <button type="button" @click="count++">count is {{ count }}</button>
         <p>
             Edit
@@ -30,11 +22,15 @@
         >
         in your IDE for a better DX
     </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p>Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-    .read-the-docs {
-        color: #888;
-    }
-</style>
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    defineProps<{ msg: string }>()
+
+    const count = ref(0)
+</script>
+
+<style></style>
